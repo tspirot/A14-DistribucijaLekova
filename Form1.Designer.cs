c73c6391d251ccfe4763b5ef6f012037da01e5be
Boban Blagojević <blagojevic.boban@tsp.edu.rs>
2023-04-12 01:37:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace A14_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonBrisi = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonAnaliza = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonOAplikaciji = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonIzlaz = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxNazivLeka = new System.Windows.Forms.TextBox();
+            this.textBoxProizvodjac = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(50, 50);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonBrisi,
+            this.toolStripButtonAnaliza,
+            this.toolStripButtonOAplikaciji,
+            this.toolStripButtonIzlaz});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(960, 79);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonBrisi
+            // 
+            this.toolStripButtonBrisi.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonBrisi.Image")));
+            this.toolStripButtonBrisi.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonBrisi.Name = "toolStripButtonBrisi";
+            this.toolStripButtonBrisi.Size = new System.Drawing.Size(54, 76);
+            this.toolStripButtonBrisi.Text = "Briši";
+            this.toolStripButtonBrisi.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButtonBrisi.Click += new System.EventHandler(this.toolStripButtonBrisi_Click);
+            // 
+            // toolStripButtonAnaliza
+            // 
+            this.toolStripButtonAnaliza.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAnaliza.Image")));
+            this.toolStripButtonAnaliza.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAnaliza.Name = "toolStripButtonAnaliza";
+            this.toolStripButtonAnaliza.Size = new System.Drawing.Size(62, 76);
+            this.toolStripButtonAnaliza.Text = "Analiza";
+            this.toolStripButtonAnaliza.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButtonAnaliza.Click += new System.EventHandler(this.toolStripButtonAnaliza_Click);
+            // 
+            // toolStripButtonOAplikaciji
+            // 
+            this.toolStripButtonOAplikaciji.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonOAplikaciji.Image")));
+            this.toolStripButtonOAplikaciji.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOAplikaciji.Name = "toolStripButtonOAplikaciji";
+            this.toolStripButtonOAplikaciji.Size = new System.Drawing.Size(87, 76);
+            this.toolStripButtonOAplikaciji.Text = "O aplikaciji";
+            this.toolStripButtonOAplikaciji.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButtonIzlaz
+            // 
+            this.toolStripButtonIzlaz.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonIzlaz.Image")));
+            this.toolStripButtonIzlaz.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonIzlaz.Name = "toolStripButtonIzlaz";
+            this.toolStripButtonIzlaz.Size = new System.Drawing.Size(54, 76);
+            this.toolStripButtonIzlaz.Text = "Izlaz";
+            this.toolStripButtonIzlaz.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bnrisanje izabranog leka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Naziv leka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(356, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Proizvođač";
+            // 
+            // textBoxNazivLeka
+            // 
+            this.textBoxNazivLeka.BackColor = System.Drawing.Color.MistyRose;
+            this.textBoxNazivLeka.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNazivLeka.Location = new System.Drawing.Point(145, 140);
+            this.textBoxNazivLeka.Name = "textBoxNazivLeka";
+            this.textBoxNazivLeka.ReadOnly = true;
+            this.textBoxNazivLeka.Size = new System.Drawing.Size(138, 27);
+            this.textBoxNazivLeka.TabIndex = 4;
+            // 
+            // textBoxProizvodjac
+            // 
+            this.textBoxProizvodjac.BackColor = System.Drawing.Color.MistyRose;
+            this.textBoxProizvodjac.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxProizvodjac.Location = new System.Drawing.Point(483, 140);
+            this.textBoxProizvodjac.Name = "textBoxProizvodjac";
+            this.textBoxProizvodjac.ReadOnly = true;
+            this.textBoxProizvodjac.Size = new System.Drawing.Size(138, 27);
+            this.textBoxProizvodjac.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(942, 355);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxProizvodjac);
+            this.Controls.Add(this.textBoxNazivLeka);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Lek-Proizvođač";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonBrisi;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAnaliza;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOAplikaciji;
+        private System.Windows.Forms.ToolStripButton toolStripButtonIzlaz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxNazivLeka;
+        private System.Windows.Forms.TextBox textBoxProizvodjac;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
